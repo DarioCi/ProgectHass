5006bb273ffdafd74c3058f632e8132c0da51b1b
DarioCi <dariociardielo39@gmail.com>
2022-10-25 17:28:16 +0200

    Goldsignal class

--- GoldSignal.py
+++ GoldSignal.py
@@ -5,26 +5,29 @@ import SigOp.common as cm
 from scipy import signal
 
 class Gold:
-  def __init__(self, path):
-    self.path = path
 
-  def __str__(self):
-    return f"{self.path}"
+  path = ""
 
-  def elabora(self):
-    data = np.genfromtxt(self.path, delimiter=';')
-    plt.plot(data[15000:15800], label="Segnale di partenza gold")
-    plt.legend()
-    plt.figure()
-    goldresample = signal.resample(data[15000:15400], 800)
-    plt.plot(goldresample, label="Gold da 100hz -> 50hz")
-    plt.legend()
-    goldnormalize = sg.renormalize_signal(data[15000:15800], 100)
-    goldresample2 = signal.resample(goldnormalize[:400], 800)
-    plt.plot(goldresample2, label="Gold a 50hz normalizzato")
-    plt.legend()
-    plt.show()
+  def setPath(self, elaborationPath):
+    self.path = elaborationPath
 
-    s1=Gold('C:/Users/dario/Desktop/Hassisto/Prova2/waveform-gold/gold.csv')
-    print("il path di elaborazione è: " + s1.path)
-    s1.elabora()
+  def normalizeData(self, inputData,signalfrequency):
+      return sg.renormalize_signal(inputData, signalfrequency)
+
+  def resample(self,signalfrequency,windin,windout,ncampioni):
+      data = np.genfromtxt(self.path, delimiter=';')
+      goldnormalize = sg.renormalize_signal(data[windin:windout], signalfrequency)
+      goldresample = signal.resample(goldnormalize[:(ncampioni/2)], ncampioni)
+      plt.plot(goldresample,label="Segnale normalizzato e ricampionato")
+      return goldresample
+
+  def plot(self,signal):
+      plt.plot(signal)
+      plt.show()
+
+
+
+s1 = Gold()
+s1.setPath('C:/Users/dario/Desktop/Hassisto/Prova2/waveform-gold/gold.csv')
+# print("il path di elaborazione è: " + s1.path)
+s1.resample()
